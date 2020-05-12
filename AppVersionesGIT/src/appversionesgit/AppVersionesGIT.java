4387c55d6cbc95c880a2395ce104d0cb3fd49999
Gabriel Ferreira Otero <gabyotdu@gmail.com>
2020-05-12 12:54:15 +0200

    Version 3.0

--- AppVersionesGIT/src/appversionesgit/AppVersionesGIT.java
+++ AppVersionesGIT/src/appversionesgit/AppVersionesGIT.java
@@ -16,7 +16,8 @@ public class AppVersionesGIT {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Esta es la version 2.0");
+        System.out.println("Esta es la version 3.0");
+        NuevaClaseVersion3.mensaje();
     }
     
 }
---
+++ AppVersionesGIT/src/appversionesgit/NuevaClaseVersion3.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appversionesgit;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NuevaClaseVersion3 {
+    public static void mensaje() {
+        System.out.println("Esta es la nueva funcion en la version 3.0");
+    }
+}
